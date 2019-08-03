3b17724f69c55e7354564f26f7e0432ee5b78c57
joekennedy256 <52857319+joekennedy256@users.noreply.github.com>
2019-08-03 18:37:31 +0100

    Add files via upload
    Upload

---
+++ numbergame.js
@@ -0,0 +1,20 @@
+// create secretNumber
+var secretNumber = 4;
+
+//ask user for guess
+var stringGuess = prompt("Guess a number");
+var guess = Number(stringGuess);
+ 
+
+//check if guess is right
+if(Number(guess) === secretNumber) {
+    alert("You got it right!");
+ }
+
+//otherwise, you got it wrong
+else if(Number(guess) > secretNumber) {
+    alert("Too high. Guess again!")
+}
+else {
+    alert("Too low. Guess again!")
+}
